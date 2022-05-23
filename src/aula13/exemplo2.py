a6d307823239d2890fd0aab2d6ea18a50621462c
Amorim <guh.contato.work@outlook.com.br>
2022-05-23 19:16:08 -0300

    len metodo

---
+++ src/aula13/exemplo2.py
@@ -0,0 +1,7 @@
+string1 = input('Digite alguma coisa: ')
+string2 = input('Digite outra coisa: ')
+
+print(
+    f'A quantidade total de caracteres digitados foi {len(string1) + len(string2)}')
+print(len(string2))
+print(string2.__len__())
