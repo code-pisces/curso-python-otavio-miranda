3eb4c2d2ae4cbfb874a8dde910b939e2ac5985f2
Amorim <guh.contato.work@outlook.com.br>
2022-05-30 20:14:38 -0300

    Fatiamento de strings com python.

---
+++ src/aula18/aula18.py
@@ -0,0 +1,13 @@
+"""
+Manipulando Strings
+* String indices
+* Fatiamente de strings [inicio:fim:passo]
++ Funções buil-in len, abs, type, print, etc...
+"""
+# positivos   [012345678]
+texto = 'Python_S2'
+# negativos  -[987654321]
+
+nova_string = texto[0::3]
+print(texto[2])
+print(nova_string)
