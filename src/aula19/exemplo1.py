99edea1962fb5c7b300a02be22432948bf85488f
Amorim <guh.contato.work@outlook.com.br>
2022-05-31 21:00:42 -0300

    While

---
+++ src/aula19/exemplo1.py
@@ -0,0 +1,14 @@
+"""
+while em Python
+utilizado para realizar ações enquanto a condição for verdadeira
+"""
+x = 0
+
+while x < 10:
+    y = 0
+    while y < 5:
+        print(f'X vale {x} e Y vale {y}')
+        y += 1
+    x = + 1  # x = x + 1
+
+print('Acabou!')
---
+++ src/aula19/exemplo2.py
@@ -0,0 +1,30 @@
+from ast import operator
+
+
+while True:
+    print()
+    num_1 = input('Digite um número: ')
+    num_2 = input('Digite outro número: ')
+    operator = input('Digite um operador: ')
+    sair = input('Deseja sair? [s]im ou [n]ão:')
+
+    if sair == 's':
+        break
+
+    if not num_1.isnumeric() or not num_2.isnumeric():
+        print('Você precisa digitar um número ou sair.')
+        continue
+
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+
+    # +-/*
+
+    if operator == '+':
+        print(num_1 + num_2)
+    elif operator == '-':
+        print(num_1 - num_2)
+    elif operator == '/':
+        print(num_1 / num_2)
+    else:
+        print(num_1 * num_2)
